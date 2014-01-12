71c2b0070d3a291771f458b24c52b5f1405488fa
tna20141 <anhlq2110@gmail.com>
2014-01-12 22:32:07 +0700

    add basic stack

---
+++ basic/include/basic_stack.h
@@ -0,0 +1,37 @@
+#ifndef _BASIC_STACK_H
+#define _BASIC_STACK_H
+
+/*
+ * Simple FIFO stack implementation.
+ *
+ */
+
+struct basic_stack_list_head {
+	struct basic_stack_list_head *next;
+};
+typedef struct basic_stack_list_head bs_elem;
+
+struct basic_stack {
+	bs_elem *top;
+	int num;
+};
+
+/*
+ * API functions
+ */
+static inline void basic_stack_init(struct basic_stack *stack);
+
+static inline int basic_stack_is_empty(struct basic_stack *stack);
+
+static inline int basic_stack_num_elem(struct basic_stack *stack);
+
+/*
+ * private functions
+ */
+static inline void __basic_stack_push(bs_elem *elem, struct basic_stack *stack);
+
+static inline bs_elem *__basic_stack_pop(struct basic_stack *stack);
+
+s
+
+#endif
